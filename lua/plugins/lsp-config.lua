0717ace98e747a83731b5d879e2ae67e0e1d8066
Juan José Prego <juancho.prego.gundin@gmail.com>
2024-03-18 04:54:10 +0100

    Completions and help for coding

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,32 +1,40 @@
 return {
-  {
-    "williamboman/mason.nvim",
-    config = function()
-      require("mason").setup({
-        ensure_installed = {"pyright"}
-      })
-    end
-  },
-  {
-    "williamboman/mason-lspconfig.nvim",
-    config = function()
-      require("mason-lspconfig").setup({
-        ensure_installed = {"lua_ls"}
-      })
-    end
-  },
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      local lspconfig = require("lspconfig", "tsserver","pyright", "hls")
-      lspconfig.lua_ls.setup({})
-      lspconfig.tsserver.setup({})
-      lspconfig.pyright.setup({})
-      lspconfig.hls.setup({})
+   {
+      "williamboman/mason.nvim",
+      lazy = false,
+      config = function()
+         require("mason").setup({})
+      end,
+   },
+   {
+      "williamboman/mason-lspconfig.nvim",
+      lazy = false,
+      opts = {
+         auto_install = true,
+      },
+   },
+   {
+      "neovim/nvim-lspconfig",
+      lazy = false,
+      config = function()
 
-      vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
-      vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
-      vim.keymap.set({'n', 'v'}, '<leader>ca', vim.lsp.buf.code_action, {})
-    end
-  }
+         local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
+         local lspconfig = require("lspconfig", "tsserver", "pyright", "hls")
+         lspconfig.lua_ls.setup({
+            capabilities = capabilites
+         })
+         lspconfig.tsserver.setup({
+            capabilities = capabilities
+         })
+         lspconfig.pyright.setup({
+            capabilities = capabilities
+         })
+         lspconfig.hls.setup({})
+
+         vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+         vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+         vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+      end,
+   },
 }
