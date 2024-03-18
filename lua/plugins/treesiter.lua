73831e8cfdc3b09068a44e9fc240002e061cf008
Juan José Prego <juancho.prego.gundin@gmail.com>
2024-03-18 03:58:07 +0100

    Formatting features null-ls

--- lua/plugins/treesiter.lua
+++ lua/plugins/treesiter.lua
@@ -1,12 +1,13 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  build = ":TSUpdate",
-  config = function()
-    local config = require("nvim-treesitter.configs")
-    config.setup({
-      ensure_installed = {"lua", "haskell", "javascript", "python", "c", "rust"},
-      highlight = { enable = true },
-      indent = { enable = true},
-    })
-  end
+   "nvim-treesitter/nvim-treesitter",
+   build = ":TSUpdate",
+   config = function()
+      local config = require("nvim-treesitter.configs")
+      config.setup({
+         autoinstall = true,
+         ensure_installed = { "lua", "haskell", "javascript", "python", "c", "rust" },
+         highlight = { enable = true },
+         indent = { enable = true },
+      })
+   end,
 }
