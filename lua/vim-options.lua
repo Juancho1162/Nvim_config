db89db09597195e69cf9029719da89983795995b
Juan José Prego Gundín <juancho.prego.gundin@gmail.com>
2024-03-19 23:19:25 +0100

    Added yank compatibility with windows clippboard

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -6,3 +6,13 @@ vim.cmd("set relativenumber")
 vim.cmd("set number")
 vim.g.mapleader = " "
 vim.opt.termguicolors = true
+
+local clip_path = '/mnt/c/Windows/System32/clip.exe'
+if vim.fn.executable(clip_path) == 1 then
+   vim.cmd([[
+    augroup WSLYank
+      autocmd!
+      autocmd TextYankPost * call system(']] .. clip_path .. [[', @0)
+    augroup END
+  ]])
+end
